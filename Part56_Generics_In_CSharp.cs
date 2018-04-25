54bdfb7716fdcccea63cc702dfe7b2fafa7e2e41
KimaniWaNdirangu <kimani.wa.ndirangu@gmail.com>
2018-04-25 08:13:45 +0300

    Added Part56_Generics_In_CSharp.cs

---
+++ Part56_Generics_In_CSharp.cs
@@ -0,0 +1,29 @@
+using System;
+
+// Part 56 - Generics in C#
+public class Program
+{
+    private static void Main()
+    {
+        bool Equal = Calculator.AreEqual<int>(10, 10);
+
+        if (Equal)
+        {
+            Console.WriteLine("Equal");
+        }
+        else
+        {
+            Console.WriteLine("Not Equal");
+        }
+    }
+}
+
+public class Calculator
+{
+    public static bool AreEqual<T>(T Value1, T Value2)
+    {
+        return Value1.Equals(Value2);
+    }
+}
+
+
