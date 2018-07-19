d70c14f20f613c726cf28e7bb628ee4a250a36c1
KimaniWaNdirangu <kimani.wa.ndirangu@gmail.com>
2018-07-19 07:44:53 +0300

    Added 2 files

---
+++ Part04_String_Types.cs
@@ -0,0 +1,16 @@
+using System;
+// Part 04 - String Types in C Sharp
+class Program
+{
+    static void Main()
+    {
+        //string strNormalString = "Pragim";
+        //string strNewLineBreak = "One\nTwo\nThree";
+
+        string strVerbatimLiteral = @"C:\Pragim\DotNet\Training\Csharp";
+        Console.WriteLine(strVerbatimLiteral);
+        
+    }
+}
+
+
