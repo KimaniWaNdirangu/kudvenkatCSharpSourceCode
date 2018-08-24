eae9df0fe9aa5dcb37df9025760b832e0a158042
KimaniWaNdirangu <kimani.wa.ndirangu@gmail.com>
2018-08-24 20:15:35 +0300

    Added Code

--- Part05_Common_Operators.cs
+++ Part05_Common_Operators.cs
@@ -1,5 +1,5 @@
 using System;
-//Part 05 - Commone Operators in C
+// Part 05 - Commone Operators in C
 // Assignment Operator      =
 // Arithmetic Operatiors    +. -, *, /, %
 // Comparison Operator      ==, !=, >, >= , <=
@@ -18,20 +18,39 @@ class Program
 
         //Console.WriteLine("Result = {0}", Result);
 
-        int Number = 10;
-        int AnotherNumber = 20;
+        //int Number = 10;
+        //int AnotherNumber = 21;
 
         //Exclusive AND
-        if (Number == 10 && AnotherNumber == 20)
-        {
-            Console.WriteLine(Number);
-        }
+        //if (Number == 10 && AnotherNumber == 20)
+        //{
+        //    Console.WriteLine("Hello");
+        //}
 
         //Exclusiove OR
-        if (Number == 10 || AnotherNumber == 20)
-        {
-            Console.WriteLine(Number);
-        }
+        //if (Number == 10 || AnotherNumber == 20)
+        //{
+        //    Console.WriteLine("Hello");
+        //}
+
+        //Ternary Operator
+
+        int Number = 10;
+
+        //bool isNumber10;
+
+        //if (Number == 10)
+        //{
+        //    isNumber10 = true;
+        //}
+        //else
+        //{
+        //    isNumber10 = false;
+        //}
+
+        bool isNumber10 = Number == 10 ? true : false;
+
+        Console.WriteLine("Number == 10 is {0}", isNumber10);
     }
 }
 
